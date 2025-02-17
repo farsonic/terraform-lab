57048e07bcaa23dff4d1d12debea2c8ec37a3ebb
Farsonic <fp@prowsehouse.com>
2025-02-17 12:06:21 +1000

    added ability to iterate through VLAN's for containers

--- terraform_docker/docker.tf
+++ terraform_docker/docker.tf
@@ -1,65 +1,113 @@
-variable "container_count" {
-  default = 10
+variable "container_config" {
+  type = map(number)
+  default = {
+    "vlan_10" = 0  # VLAN 10 -> 2 containers
+    "vlan_20" = 10  # VLAN 20 -> 3 containers
+  }
 }
 
-# Ensure VLAN & Subnet exist before reserving addresses
-data "phpipam_subnet" "subnet_10" {
-  subnet_address = "192.168.10.0"
-  subnet_mask    = 24
-  depends_on     = [phpipam_subnet.subnet_10]
+locals {
+  container_instances = flatten([
+    for vlan, count in var.container_config : [
+      for i in range(count) : {
+        vlan   = vlan
+        number = i + 1
+      }
+    ]
+  ])
 }
 
-# **Step 1: Fetch & Reserve Unique Free IPs from phpIPAM**
+
+
+
+
+
+
+# ------------- Change nothing below this -----------------
+# **Fetch subnet details dynamically per VLAN**
+data "phpipam_subnet" "subnets" {
+  for_each   = var.container_config
+  depends_on = [phpipam_subnet.subnets]
+  subnet_id  = phpipam_subnet.subnets[each.key].subnet_id
+}
+
+# **Fetch pre-allocated gateway IP per VLAN**
+data "phpipam_address" "gateway_ips" {
+  for_each   = var.container_config
+  depends_on = [phpipam_address.gateway_ips]
+  ip_address = phpipam_address.gateway_ips[each.key].ip_address
+}
+
+# **Request unique free IPs for containers per VLAN**
 resource "phpipam_first_free_address" "reserved_ips" {
-  count = var.container_count
+  for_each = { for instance in local.container_instances : "${instance.vlan}-${instance.number}" => instance }
 
-  subnet_id   = phpipam_subnet.subnet_10.subnet_id
-  hostname    = format("docker-container-%d", count.index + 1)
+  subnet_id   = phpipam_subnet.subnets[each.value.vlan].subnet_id
+  hostname    = format("docker-container-%s-%d", each.value.vlan, each.value.number)
   description = "Managed by Terraform"
-  depends_on  = [phpipam_subnet.subnet_10, phpipam_address.gateway_ip]
+  depends_on  = [phpipam_subnet.subnets, phpipam_address.gateway_ips]
 }
 
-# **Step 2: Create Docker macvlan network**
-resource "docker_network" "macvtap_network" {
-  name   = "my_macvtap_net"
+# **Create a lookup map to track which container got which IP**
+locals {
+  container_ip_map = {
+    for key, ip in phpipam_first_free_address.reserved_ips :
+    key => merge(ip, { vlan = regex("^(.*)-", key)[0], number = regex("-(\\d+)$", key)[0] })
+  }
+}
+
+# **Dynamically create the Docker macvlan network per VLAN**
+resource "docker_network" "macvtap_networks" {
+  for_each = var.container_config
+
+  name   = format("my_macvtap_net_%s", each.key)
   driver = "macvlan"
 
   ipam_config {
-    subnet  = "192.168.10.0/24"
-    gateway = "192.168.10.1"
+    subnet  = format("%s/%d", phpipam_subnet.subnets[each.key].subnet_address, phpipam_subnet.subnets[each.key].subnet_mask)
+    gateway = phpipam_address.gateway_ips[each.key].ip_address
   }
 
   options = {
-    parent = "vlan.10"
-    mode   = "private"
+    parent = format("vlan.%s", replace(each.key, "vlan_", ""))  # Extract VLAN ID dynamically
+    macvlan_mode   = "private"
   }
 }
 
-# **Step 3: Pull Alpine Image**
+# **Pull Alpine Image**
 resource "docker_image" "alpine" {
   name = "alpine:latest"
 }
 
-# **Step 4: Ensure Each Container Gets a Unique IP**
+# **Create Docker Containers with Unique IPs per VLAN**
 resource "docker_container" "containers" {
-  for_each = { for idx, ip in phpipam_first_free_address.reserved_ips : idx => ip }
+  for_each = local.container_ip_map
 
-  name    = format("docker-container-%d", each.key + 1)
+  name    = format("docker-container-%s-%s", each.value.vlan, each.value.number)
   image   = docker_image.alpine.image_id
   restart = "always"
   command = ["sleep", "infinity"]
+  network_mode = "bridge"
 
-  # Attach container to macvlan network with reserved IP
+  # Attach container to the corresponding VLAN's macvlan network
   networks_advanced {
-    name         = docker_network.macvtap_network.name
+    name         = docker_network.macvtap_networks[each.value.vlan].name
     ipv4_address = each.value.ip_address
   }
-
-  depends_on = [phpipam_first_free_address.reserved_ips]
 }
 
-# **Step 5: Output Allocated IPs**
+# **Outputs**
 output "allocated_ips" {
-  value       = [for ip in phpipam_first_free_address.reserved_ips : ip.ip_address]
+  value       = { for key, ip in phpipam_first_free_address.reserved_ips : key => ip.ip_address }
   description = "The Allocated IP Addresses from PHPIPAM"
+}
+
+output "gateway_ips" {
+  value       = { for key, ip in phpipam_address.gateway_ips : key => ip.ip_address }
+  description = "The Allocated Gateway IPs from PHPIPAM"
+}
+
+output "subnet_info" {
+  value       = { for key, subnet in phpipam_subnet.subnets : key => format("%s/%d", subnet.subnet_address, subnet.subnet_mask) }
+  description = "The Allocated Subnets and Masks from PHPIPAM"
 }
--- terraform_docker/vlan.tf
+++ terraform_docker/vlan.tf
@@ -1,25 +1,55 @@
+variable "vlans" {
+  type = map(object({
+    vlan_number  = number
+    subnet       = string
+    subnet_mask  = number
+    gateway_ip   = string
+  }))
+
+  default = {
+    "vlan_10" = {
+      vlan_number = 10
+      subnet      = "192.168.10.0"
+      subnet_mask = 24
+      gateway_ip  = "192.168.10.1"
+    }
+    "vlan_20" = {
+      vlan_number = 20
+      subnet      = "192.168.20.0"
+      subnet_mask = 24
+      gateway_ip  = "192.168.20.1"
+    }
+  }
+}
+
+
+# ------------- Change nothing below this -----------------
+# Create a Lab section where VLANs and Subnets are attached 
 resource "phpipam_section" "lab" {
   name        = "Lab"
-  description = "Terraform test section"
+  description = "Terraform Lab"
 }
 
-resource "phpipam_vlan" "vlan_10" {
-  name       = "VLAN 10"
-  number     = 10
-  description = "Terraform-managed VLAN 10"
+resource "phpipam_vlan" "vlans" {
+  for_each    = var.vlans
+  name        = "VLAN ${each.value.vlan_number}"
+  number      = each.value.vlan_number
+  description = "VLAN ${each.value.vlan_number}"
 }
 
-resource "phpipam_subnet" "subnet_10" {
-  subnet_address = "192.168.10.0"
-  subnet_mask    = 24
-  description    = "Terraform-managed subnet for VLAN 10"
-  vlan_id        = phpipam_vlan.vlan_10.vlan_id 
+resource "phpipam_subnet" "subnets" {
+  for_each       = var.vlans
+  subnet_address = each.value.subnet
+  subnet_mask    = each.value.subnet_mask
+  description    = "Terraform-managed subnet for VLAN ${each.value.vlan_number}"
+  vlan_id        = phpipam_vlan.vlans[each.key].vlan_id
   section_id     = phpipam_section.lab.section_id
 }
 
-resource "phpipam_address" "subnet_10_gateway_ip" {
-  subnet_id   = phpipam_subnet.subnet_10.subnet_id
-  ip_address  = "192.168.10.1"
-  hostname    = "gateway"
-  description = "Reserved gateway IP"
+resource "phpipam_address" "gateway_ips" {
+  for_each    = var.vlans
+  subnet_id   = phpipam_subnet.subnets[each.key].subnet_id
+  ip_address  = each.value.gateway_ip
+  hostname    = "gateway-${each.value.vlan_number}"
+  description = "Reserved gateway IP for VLAN ${each.value.vlan_number}"
 }
